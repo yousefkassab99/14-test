931775f95539ea9397497c3accaf4ce7158e10b1
yousef kassab <yousefkassabw.99@gmail.com>
2022-12-22 12:31:56 +0300

    ch

--- src/componants/Count/Index.jsx
+++ src/componants/Count/Index.jsx
@@ -1,4 +1,5 @@
 
+
 import "./index.css";
 import React, { Component } from "react";
 export default class Counter extends Component {
@@ -6,8 +7,8 @@ export default class Counter extends Component {
     return (
       <div className="div1">
         <div className="div2">{this.props.count}</div>
-        <div className="increment" onClick={() => this.props.increment(this.props.id, this.props.steps)}>+</div>
-        <div className="decrement"onClick={() => this.props.decrement(this.props.id, this.props.steps)}>-</div>
+        <div className="increment" onClick={() => this.props.increment(this.props.id, this.props.x)}>+</div>
+        <div className="decrement"onClick={() => this.props.decrement(this.props.id, this.props.x)}>-</div>
       </div>
     );
   }
